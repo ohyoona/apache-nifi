1b0834a1d8c412c55f9dc5be355ee8bfd93ec0e3
yaoh <yoona3827@naver.com>
2026-01-26 10:59:30 +0900

    커밋

---
+++ apache-nifi-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1. dev 브랜치 Pull
+echo "Updating dev branch..."
+git checkout dev
+git pull origin dev
+
+# 2. NiFi Process Group ID
+NIFI_PG_ID="d4f982c8-019b-1000-1552-51aad59345a2"
+NIFI_URL="http://localhost:8882/nifi-api"
+
+# 3. NiFi Update Version 요청
+echo "Triggering NiFi Update Version..."
+curl -X POST "${NIFI_URL}/versions/process-groups/${NIFI_PG_ID}/update-requests" \
+     -H 'Content-Type: application/json' \
+     -d '{}'
+
+echo "NiFi dev Process Group update requested."
